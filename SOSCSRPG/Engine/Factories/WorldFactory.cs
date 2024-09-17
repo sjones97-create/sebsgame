0b49adb441d403c6ba4fc77947bab935683cc5c7
sjones97-create <sebastian.jones.97@gmail.com>
2024-09-17 21:14:02 +1000

    yes
    yes

---
+++ SOSCSRPG/Engine/Factories/WorldFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal static class WorldFactory
+    {
+        internal static World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(0,
+                                 -1,
+                                 "Home",
+                                 "This is your home",
+                                 "pack://application:,,,/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1,
+                                 -1,
+                                 "Farmer's House",
+                                 "This is the house of your neighbour, Farmer Ted.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(-2, 
+                                 -1,
+                                 "Farmer's Field",
+                                 "There are rows of corn growing here, with giant rats hiding between them.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(-1,
+                                 0,
+                                 "Trading Shop",
+                                 "The shop of Susan, the trader",
+                                 "pack://application:,,,/Engine;component/Images/Locations/Trader.png");
+
+            newWorld.AddLocation(0,
+                                 0,
+                                 "Town Square",
+                                 "You see a fountain here.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/TownSquare.png");
+
+            newWorld.AddLocation(1,
+                                 0,
+                                 "Town Gate",
+                                 "There is a gate here, protecting the town from giant spiders.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/TownGate.png");
+
+            newWorld.AddLocation(2,
+                                 0,
+                                 "Spider Forest",
+                                 "The trees in this forest are covered with spider webs.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/SpiderForest.png");
+
+            newWorld.AddLocation(0,
+                                 1,
+                                 "Herbalist's Hut",
+                                 "You see a small hut, with plants drying from the roof.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/HerbalistsHut.png");
+           
+            newWorld.LocationAt(0, 1).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(1));
+
+            newWorld.AddLocation(0,
+                                 2,
+                                 "Herbalist's Garden",
+                                 "There are many plants here, with snakes hiding behind them.",
+                                 "pack://application:,,,/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+
+
+
+
+            return newWorld;
+        }
+    }
+}
